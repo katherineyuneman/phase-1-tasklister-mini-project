41e16c486e25cd19d3d2e528946bfdc13e4d406d
KatherineYuneman <kyuneman@gmail.com>
2021-11-06 12:46:05 -0400

    Completed MVP deliverables not stretch deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+document.querySelector('form').addEventListener('submit', e => {
+  e.preventDefault();
+  buildToDo(e.target.new_task_description.value);
+
+})
+
+
+})
+
+function buildToDo (todo){
+  let li = document.createElement('li');
+  li.textContent = todo
+  let btn = document.createElement('button')
+  li.appendChild(btn)
+  btn.textContent = ' x '
+  btn.addEventListener('click', handleDelete)
+
+  document.querySelector('#tasks').appendChild(li);
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove();
+}
